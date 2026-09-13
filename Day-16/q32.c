2de7b41a8cd4ed929eba43d3fbeaa5804176add2
mayrabisht2007 <mayrabisht2007@gmail.com>
2026-09-13 07:46:26 +0530

    Add palindrome check program in q32.c
    This program checks if a given integer is a palindrome by reversing the number and comparing it to the original.

---
+++ Day-16/q32.c
@@ -0,0 +1,39 @@
+//Q32: Write a program to check if a number is a palindrome.
+
+/*
+Sample Test Cases:
+Input 1:
+121
+Output 1:
+Palindrome
+
+Input 2:
+123
+Output 2:
+Not palindrome
+
+*/
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num > 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num = num / 10;
+    }
+
+    if (original == reversed) {
+        printf("Palindrome\n");
+    } else {
+        printf("Not palindrome\n");
+    }
+
+    return 0;
+}
